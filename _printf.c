e80205303580d0372eaaa7daba34079dcc186089
Akinola-Moses <akinolamosesmyself@gmail.com>
2023-09-18 00:25:44 +0000

    Print output to stdout base on format

--- _printf.c
+++ _printf.c
@@ -13,7 +13,8 @@ int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
 	int format_c = 0, idx, c_idx = 0;
-	int c;
+	int c = 0, integer = 0, calc_l = 0;
+	char integer_s[24];
 	char *s;
 	va_list print_me;
 
@@ -39,6 +40,18 @@ int _printf(const char *format, ...)
 				c_idx++;
 				format_c++;
 			}
+			else if (format[format_c] == 'd' || format[format_c] == 'i')
+			{
+				format++;
+				integer = va_arg(print_me, int);
+				calc_l = snprintf(integer_s, sizeof(integer_s), "%d", integer);
+				if ((calc_l < 0) || (calc_l >= (int)sizeof(integer_s)))
+				{
+					return (-1);
+				}
+				write(1, integer_s, calc_l);
+				c_idx += calc_l;
+			}
 			else if (format[format_c] == 'c')
 			{
 				c = va_arg(print_me, int);
@@ -60,4 +73,11 @@ int _printf(const char *format, ...)
 	}
 	va_end(print_me);
 	return (c_idx);
+}
+int main(void)
+{
+	_printf("me");
+	_printf("%c", 'a');
+	_printf("%d, %i", 2, 4);
+	return (0);
 }
